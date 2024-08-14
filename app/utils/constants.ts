d565849d830757fce1477d63df4fbbcb473c2cb2
kizmt <kizmetsol@proton.me>
2024-08-14 13:48:36 +1200

    Update constants.ts

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -1,12 +1,17 @@
-import { PublicKey } from '@solana/web3.js';
-import { BN } from '@coral-xyz/anchor';
+import { PublicKey } from "@solana/web3.js";
+import { BN } from "@coral-xyz/anchor";
 
-export const ARCANA_DATA_API_URL = 'https://prod.arcana.markets/api/openbookv2/markets'
+export const ARCANA_DATA_API_URL =
+  "https://prod.arcana.markets/api/openbookv2/markets";
 
-export const WRAPPED_SOL_MINT = new PublicKey('So11111111111111111111111111111111111111112');
+export const WRAPPED_SOL_MINT = new PublicKey(
+  "So11111111111111111111111111111111111111112"
+);
 
-export const TOKEN_PROGRAM_ID = new PublicKey('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA');
+export const TOKEN_PROGRAM_ID = new PublicKey(
+  "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"
+);
 
-export const DEFAULT_MARKET_ID = 'CFSMrBssNG8Ud1edW59jNLnq2cwrQ9uY5cM3wXmqRJj3';
+export const DEFAULT_MARKET_ID = "AFgkED1FUVfBe2trPUDqSqK9QKd4stJrfzq5q1RwAFTa";
 
 export const BN_0 = new BN(0);
